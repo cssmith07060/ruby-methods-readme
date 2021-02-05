a8f94093a8464a6e3d1b4d604c9cded891262037
cssmith07060 <66744380+cssmith07060@users.noreply.github.com>
2021-02-04 21:45:03 -0500

    Add files via upload

---
+++ greeting.rb
@@ -0,0 +1,32 @@
+# Use this space to code along with the readme
+def say_hello_ten_times
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
+say_hello_ten_times
+def greeting
+    puts "Hello World"
+end
+
+def greeting
+
+end
+
+def greeting
+    puts "Hello World"
+end
+
+def greeting
+    puts "Hello World"
+    end
+
+
